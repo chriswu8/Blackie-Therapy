41ba4e08b37ccc2cbef9c06398eb3886ad4e962c
Chris Wu <zezemint@gmail.com>
2022-08-23 18:37:12 -0700

    deployed to heroku

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,10 +1,11 @@
 import React, { createContext, useState, useRef, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://warm-wildwood-81069.herokuapp.com');
+const socket = io('https://blackietherapy.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
